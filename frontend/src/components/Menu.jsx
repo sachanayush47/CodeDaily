1cfb419f00942868aec83eec8a835349701db9f3
Ayush Sachan <sachan.ayush47@gmail.com>
2022-11-09 00:15:54 +0530

    Designed write page, new favicon icon

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -32,7 +32,7 @@ const Menu = () => {
         <div className="menu">
             <h1>Otherposts you may like</h1>
             {posts.map((post) => (
-                <div className="post">
+                <div className="post" key={post.id}>
                     <img src={post.img} alt="" />
                     <h2>{post.title}</h2>
                     <button>read more...</button>
--- frontend/src/pages/Write.jsx
+++ frontend/src/pages/Write.jsx
@@ -1,7 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
+
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 function Write() {
-    return <div>Write</div>;
+    const [value, setValue] = useState("");
+
+    return (
+        <div className="add">
+            <div className="content">
+                <input type="text" placeholder="Title" />
+                <div className="editorContainer">
+                    <ReactQuill
+                        className="editor"
+                        theme="snow"
+                        value={value}
+                        onChange={setValue}
+                        placeholder="Start writing here..."
+                    />
+                </div>
+            </div>
+            <div className="menu">
+                <div className="item">
+                    <h1>Publish</h1>
+                    <span>Status: draft</span>
+                    <span>Visibility: public</span>
+                    <input style={{ display: "none" }} type="file" id="file" />
+
+                    <div className="buttons">
+                        <button>
+                            <label htmlFor="file">Upload image</label>
+                        </button>
+                        <button>Save as draft</button>
+                        <button>Update</button>
+                    </div>
+                </div>
+                <div className="item">
+                    <h1>Category</h1>
+
+                    <div className="cat">
+                        <input type="radio" name="cat" value="dsa" id="dsa" />
+                        <label htmlFor="dsa">DSA</label>
+                    </div>
+                    <div className="cat">
+                        <input
+                            type="radio"
+                            name="cat"
+                            value="webdev"
+                            id="webdev"
+                        />
+                        <label htmlFor="webdev">WebDev</label>
+                    </div>
+                    <div className="cat">
+                        <input
+                            type="radio"
+                            name="cat"
+                            value="languages"
+                            id="languages"
+                        />
+                        <label htmlFor="languages">Languages</label>
+                    </div>
+                    <div className="cat">
+                        <input type="radio" name="cat" value="sql" id="sql" />
+                        <label htmlFor="sql">SQL</label>
+                    </div>
+                    <div className="cat">
+                        <input
+                            type="radio"
+                            name="cat"
+                            value="tricks"
+                            id="tricks"
+                        />
+                        <label htmlFor="tricks">Tricks</label>
+                    </div>
+                    <div className="cat">
+                        <input
+                            type="radio"
+                            name="cat"
+                            value="others"
+                            id="others"
+                        />
+                        <label htmlFor="others">Others</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Write;
